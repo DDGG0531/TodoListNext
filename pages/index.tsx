0bb39c46bf4f9173943579b43e50ca1eaf81b78e
JimDong <jimdong@damaiapp.com.tw>
2022-03-01 15:38:25 +0800

    fix: accurate ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
-import type { NextPage } from "next";
+import type {
+  NextPage,
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+} from "next";
 import { PageTitle } from "../components/Layout";
 import TodoList from "../components/TodoList";
 
 import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const dev = process.env.NODE_ENV !== "production";
 const server = dev ? "http://localhost:3000" : "";
 const URL = `${server}/api/todo`;
 
-const Home: NextPage = ({ fallbackData }) => {
+const Home: NextPage = ({
+  fallbackData,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const { data: todos, error } = useSWR(URL, fetcher, { fallbackData });
   return (
     <>
@@ -20,11 +26,11 @@ const Home: NextPage = ({ fallbackData }) => {
   );
 };
 
-export async function getServerSideProps() {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   console.log("getServerSideProps start");
   const data = await fetcher(URL);
   console.log("getServerSideProps", data);
   return { props: { fallbackData: data } };
-}
+};
 
 export default Home;
